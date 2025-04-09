1f042d2846d49158619ddc8f82fcd87ae7eaaeb1
CoffeeCat19 <H0neyCatCoffeeGo@proton.me>
2025-04-09 10:55:48 +0500

    The Vue files are regressing

--- web/src/components/Navbar.vue
+++ web/src/components/Navbar.vue
@@ -1,5 +1,3 @@
-<!-- src/components/Navbar.vue -->
-
 <script setup>
 import { useAuthStore } from "@/stores/authStore";
 import { useRouter } from "vue-router";
@@ -41,12 +39,6 @@ onMounted(async () => {
       </template>
     </div>
 
-    <!-- Mobile Menu Button -->
-    <button class="md:hidden text-gray-700 focus:outline-none">
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-      </svg>
-    </button>
 
   </nav>
 </template>
